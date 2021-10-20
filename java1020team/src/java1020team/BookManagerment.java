83152c01c5228b455be8bade6eea18ef56d4cf11
Galahorn <kpgyp18@naver.com>
2021-10-20 14:39:45 +0900

    저장 안해서 미안해

--- java1020team/src/java1020team/BookManagerment.java
+++ java1020team/src/java1020team/BookManagerment.java
@@ -1,8 +1,28 @@
 package java1020team;
 
 public class BookManagerment {
- 
+
 	BookDAO dao = new BookDAO();
- 
- 
+
+	public void addBook() {
+
+	}
+
+	public String selectBook() {
+
+		return null;
+	}
+
+	public void selectAll() {
+
+	}
+	
+	public void updateBook(String , int ) {
+
+		
+	}
+	
+	public void deleteBook(String ) {
+		
+	}
 }
