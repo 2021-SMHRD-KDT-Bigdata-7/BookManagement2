9ab1d6a905f9e2fdc96dbdef639f63d61f065db8
smhrd <smhrd@DESKTOP-BKU4BE1>
2021-10-20 15:49:11 +0900

    최종

--- java1020team/src/java1020team/BookDAO.java
+++ java1020team/src/java1020team/BookDAO.java
@@ -22,18 +22,12 @@ public class BookDAO {
 
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-
-			// 2. DB 연결
-
 			String url = "jdbc:oracle:thin:@192.168.1.230:1521:xe";
 			String user = "hr";
 			String password = "hr";
-
-			// url, user, password
 			conn = DriverManager.getConnection(url, user, password);
 
 		} catch (Exception e) {
-			// Excption = 어떠한 오류라도 잡아줄수 있는 에러 모음 총집합.
 			System.out.println("연결오류");
 			e.printStackTrace(); // => 오류메세지 출력해주는 메소드. 무엇에 대한 오류메세지인가 출력해줌.
 
@@ -65,7 +59,7 @@ public class BookDAO {
 
 		getConnect();
 
-		sql = "insert into member2 values(?,?,?,?,?)";
+		sql = "insert into Book values(?,?,?,?,?)";
 
 		try {
 			psmt = conn.prepareStatement(sql);
@@ -85,15 +79,15 @@ public class BookDAO {
 		return cnt;
 	}
 
-	public void select() {
+	public void select(String id) {
 		
 		getConnect();
 
 		try {
-			sql = "select*from book where b_id=?";
+			sql = "select * from book where b_id=?";
 
 			psmt = conn.prepareStatement(sql);
-			// result를 rs로 축약했습니다.
+			psmt.setString(1, id);
 			rs = psmt.executeQuery();
 
 			while (rs.next()) {
